d3d7e84d2840389fb6b60e886257f7e2158fa37c
LucasSpinelli003 <lucaszk003@gmail.com>
2024-05-06 01:08:28 -0300

    [Feature] -  detail dto - listing  all cupoms

--- src/main/java/br/com/fiap/GerenciamentoPedido/dto/usuario/DetalhesUsuarioDto.java
+++ src/main/java/br/com/fiap/GerenciamentoPedido/dto/usuario/DetalhesUsuarioDto.java
@@ -1,12 +1,15 @@
 package br.com.fiap.GerenciamentoPedido.dto.usuario;
 
+import br.com.fiap.GerenciamentoPedido.dto.cupom.DetalhesCupomDto;
+import br.com.fiap.GerenciamentoPedido.model.Cupom;
 import br.com.fiap.GerenciamentoPedido.model.Usuario;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
-public record DetalhesUsuarioDto(Long idUsuario,Long idDetalhesUsuario, String nome, String cpf,String email,
-                                 String telefone, LocalDate dataNascimento ) {
+public record DetalhesUsuarioDto(Long idUsuario, Long idDetalhesUsuario, String nome, String cpf, String email,
+                                 String telefone, LocalDate dataNascimento, List<DetalhesCupomDto> cupoms) {
     public DetalhesUsuarioDto(Usuario usuario){
         this(usuario.getIdUsuario(),
                 usuario.getDetalhesUsuario().getIdDetalhesUsuario(),
@@ -14,6 +17,7 @@ public record DetalhesUsuarioDto(Long idUsuario,Long idDetalhesUsuario, String n
                 usuario.getCpf(),
                 usuario.getDetalhesUsuario().getEmail(),
                 usuario.getDetalhesUsuario().getTelefone(),
-                usuario.getDetalhesUsuario().getDataNascimento());
+                usuario.getDetalhesUsuario().getDataNascimento(),
+                usuario.getCupoms().stream().map(DetalhesCupomDto::new).toList());
     }
 }
